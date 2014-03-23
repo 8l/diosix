94180461f543a7f7fbc3fdf6bfd4d500d99a76c7
Chris Williams <diodesign@gmail.com>
2014-03-23 02:25:20 +0000

    * x86-64 PC boot GDT loaded * x86-64 PC simple boot debug added

--- hardware/pc/start.s
+++ hardware/pc/start.s
@@ -33,6 +33,7 @@ MULTIBOOT_MAGIC     equ 0xe85250d6
 
 bits 32
 
+; -------------------------------------------------------------------
 ; include necessary messy magic to keep Grub2 bootloader happy. we're loaded
 ; as a 32-bit x86 ELF executable, then we quickly switch to 64-bit mode
 section .multibootheader
@@ -62,11 +63,18 @@ multibootheader:
     dd 8    ; this tag is 8 bytes in size
 multibootheader_end:
 
+; -------------------------------------------------------------------
 section .boot
 bits 32
 align 8
+; kernel entry point from the multiboot bootloader
+; 32bit protected mode using bootloader GDT
+; 
+; => eax = multiboot magic number
+;    ebx = phys addr of multiboot data
 start:
 
+; debug: clear the textmode screen
 clear_screen:
     mov  eax, 0x000b8000
     xor  ebx, ebx
@@ -76,7 +84,57 @@ clear_screen_loop:
     cmp  eax, 0x000b8000 + (2 * (80 * 25))
     jl   clear_screen_loop
 
+    ; load our GDT, discarding the bootloader's
+    lgdt [boot_32bit_gdt_ptr]
+    mov  dx, boot_32bit_gdt_kernel_data - boot_32bit_gdt
+    mov  ds, dx
+    mov  es, dx
+    mov  ss, dx
+    mov  fs, dx
+    mov  gs, dx
+    jmp  (boot_32bit_gdt_kernel_code - boot_32bit_gdt):fixup_stack
+    
+fixup_stack:
+    nop
+    nop
+    jmp  print_and_halt
 
+; -------------------------------------------------------------------
+;   mov  ecx, (boot_32bit_gdt_kernel_code - boot_32bit_gdt)
+
+dump_hex_and_halt:
+; => ecx = value to write
+; <= doesn't return: will power off (or at least halt)
+    mov  ebx, 8
+    mov  dx, 0x3f8
+dump_hex_and_halt_hexloop:
+    mov  eax, ecx
+    shr  eax, 28
+    and  eax, 0xf
+    cmp  eax, 9
+    jle  dump_hex_and_halt_isanumber
+    add  eax, 0x41 - 10     ; character code for 'A'
+    jmp  dump_hex_and_halt_hexoutput
+dump_hex_and_halt_isanumber:
+    add  eax, 0x30          ; character code for '0'
+dump_hex_and_halt_hexoutput:
+    out  dx, al
+    shl  ecx, 4
+    sub  ebx, 1
+    cmp  ebx, 0
+    jne  dump_hex_and_halt_hexloop
+    mov  al, 0xa
+    out  dx, al
+
+; debug: attempt to force a power shutdown or halt the system
+force_dirty_power_off:
+    mov  dx, 0xb004
+    mov  ax, 0x2000
+    out  dx, ax
+    hlt
+    jmp $
+
+; debug: write a line to the textmode screen
 print_and_halt:
     mov  eax, 0x000b8000
     mov  ebx, teststring
@@ -91,8 +149,44 @@ print_loop:
     inc  ebx
     jmp  print_loop
 do_halt:
+    hlt
     jmp $
 
 teststring:
-    db 'hello, world!'
+    db 'I survived the GDT reload!'
     db 0
+
+; -------------------------------------------------------------------
+; boot gdt structures
+align 16
+boot_32bit_gdt:
+    ; null descriptor
+    dw 0      ; limit 15:0
+    dw 0      ; base 15:0
+    db 0      ; base 23:16
+    db 0      ; type
+    db 0      ; limit 19:16, flags
+    db 0      ; base 31:24
+
+boot_32bit_gdt_kernel_data:
+    dw 0xffff
+    dw 0
+    db 0
+    db 0x92   ; present, ring 0, data, expand-up, writable
+    db 0xcf   ; page-granular (4 gig limit), 32-bit
+    db 0
+
+boot_32bit_gdt_kernel_code:
+    dw 0xffff
+    dw 0
+    db 0
+    db 0x9a   ; present, ring 0, code, non-conforming, readable
+    db 0xcf   ; page-granular (4 gig limit), 32-bit
+    db 0
+boot_32bit_gdt_end:
+
+boot_32bit_gdt_ptr:
+    dw boot_32bit_gdt_end - boot_32bit_gdt - 1 ; size of gdt - 1
+    dd boot_32bit_gdt
+
+
